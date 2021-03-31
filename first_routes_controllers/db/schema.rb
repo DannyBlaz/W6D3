ff92f1865af0f7d8cb0034d79abd34402e3b4c73
Daniel <dannyblaze469@gmail.com>
2021-03-31 17:05:05 -0400

    Created Artwork table

--- first_routes_controllers/db/schema.rb
+++ first_routes_controllers/db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_194132) do
+ActiveRecord::Schema.define(version: 2021_03_31_203259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "image_url", null: false
+    t.integer "artist_id", null: false
+    t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
+    t.index ["artist_id"], name: "index_artworks_on_artist_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
   end
 
 end
