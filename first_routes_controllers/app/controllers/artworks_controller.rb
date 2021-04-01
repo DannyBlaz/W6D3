37ee384bfea6e4749465ff915eabc0507c27fe93
Daniel <dannyblaze469@gmail.com>
2021-04-01 16:05:10 -0400

    Created a artwork_controller.rb file

---
+++ first_routes_controllers/app/controllers/artworks_controller.rb
@@ -0,0 +1,37 @@
+class ArtworksController < ApplicationController
+    def index
+        render json: Artwork.all
+    end
+
+    def show
+        artwork = Artwork.find(params[:id])
+        render json: artwork
+    end
+
+    def create
+        artwork = Artwork.new(artwork_params)
+
+        if artwork.save
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    
+    def update
+        artwork = Artwork.find(params[:id])
+
+        if artwork.update
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    def artwork_params
+        params.require(:artwork).permit(:title, :image_url, :artist_id)
+    end
+
+end
--- first_routes_controllers/app/controllers/users_controller.rb
+++ first_routes_controllers/app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
         render json: User.all
     end
 
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
 
     def create
         user = User.new(user_params)
@@ -15,23 +19,15 @@ class UsersController < ApplicationController
         end
     end
 
-
-    def show
-        user = User.find(params[:id])
-        render json: user
-    end
-
-
     def update
         user = User.find(params[:id])
         if user.update(user_params)
             render json: user
         else
-            render json: user.errors.full_messages, status: :unproccessable_entity
+            render json: user.errors.full_messages, status: :unprocessable_entity
         end
     end
   
-
     def destroy
         user = User.find(params[:id])
 
