48fbf94dd390b274e4f2c01de1b809c2c2b2c70d
Daniel <dannyblaze469@gmail.com>
2021-04-01 14:46:23 -0400

    Made create and destroy controller methods

--- first_routes_controllers/app/controllers/users_controller.rb
+++ first_routes_controllers/app/controllers/users_controller.rb
@@ -5,11 +5,21 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(params.require(:user).permit(:name, :email))
+        user = User.new(user_params)
         if user.save
             render json: user
         else
             render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+
+        if user.destroy
+            render json: user
+        else
+            render json: "User can not be deleted!!!"
         end
     end
 
@@ -18,5 +28,11 @@ class UsersController < ApplicationController
         render json: params 
     end
 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 
 end
