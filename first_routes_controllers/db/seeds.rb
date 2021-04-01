44bbc8ebb252f341edb7fd8624b9e87e39a91829
Daniel <dannyblaze469@gmail.com>
2021-04-01 11:51:49 -0400

    Started seeds file

--- first_routes_controllers/db/seeds.rb
+++ first_routes_controllers/db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# def rand_id
+#   rand(1..100_000)
+# end
+
+ActiveRecord::Base.transaction do
+    ArtworkShare.delete_all
+    Artwork.delete_all
+    User.delete_all
+    
+    sam = User.create(
+        id: 1,
+        username: "sam_lilly"
+    )
+
+    daniel = User.create(
+        id: 2,
+        username: "danny_blaz"
+    )
+
+    ara = User.create(
+        id: 3,
+        username: "aratist"
+    )
+
+
+end
+
+
+
+# ActiveRecord::Base.transaction do
+#   Artist.delete_all
+#   User.delete_all
+#   Album.delete_all
+#   Playlist.delete_all
+#   Song.delete_all
+#   PlaylistTrack.delete_all
+
+#   ricky = User.create(
+#     id: rand_id,
+#     username: "ricky",
+#     password: "grade10"
+#   )
